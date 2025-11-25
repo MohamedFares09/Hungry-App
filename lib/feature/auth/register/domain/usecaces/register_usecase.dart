f75ca789904ca25cddc0f9d4317ddeff5ed5dbc4
MohamedFares09 <mofares010@gmail.com>
2025-11-25 19:07:15 +0200

    add register use case

--- lib/feature/auth/register/domain/usecaces/register_usecase.dart
+++ lib/feature/auth/register/domain/usecaces/register_usecase.dart
@@ -3,7 +3,7 @@ import 'package:hungry_app/core/networking/errors/faliuer.dart';
 import 'package:hungry_app/feature/auth/register/domain/entities/user_entity.dart';
 import 'package:hungry_app/feature/auth/register/domain/repo/register_repo.dart';
 
-class RegisterUseCase {
+ class RegisterUseCase {
   final RegisterRepo registerRepo;
 
   RegisterUseCase({required this.registerRepo});
