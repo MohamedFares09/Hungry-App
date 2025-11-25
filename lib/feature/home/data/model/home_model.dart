68857e3cc76e2b44c62f9ba28a5dc934af9af4d8
MohamedFares09 <mofares010@gmail.com>
2025-11-25 13:37:12 +0200

    add factory model in home model

--- lib/feature/home/data/model/home_model.dart
+++ lib/feature/home/data/model/home_model.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:hungry_app/feature/home/doman/entities/home_entity.dart';
 
 class HomeModel extends HomeEntity {
@@ -11,4 +12,15 @@ class HomeModel extends HomeEntity {
     required this.id,
     required this.price,
   });
+
+  factory HomeModel.fromJson(Map<String, dynamic> json) {
+    return HomeModel(
+      id: json['data']['id'] ?? 0,
+      price: json['data']['price'] ?? '',
+      title: json['data']['title'] ?? '',
+      desc: json['data']['desc'] ?? '',
+      image: json['data']['image'] ?? '',
+      rate: json['data']['rate'] ?? 0,
+    );
+  }
 }
