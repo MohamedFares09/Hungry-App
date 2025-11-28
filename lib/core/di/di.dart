6065c6b52a7e4b9ca5c023594de45732a5f7e7e2
MohamedFares09 <mofares010@gmail.com>
2025-11-28 18:21:11 +0200

    feat: setup dependency injection for core services
    - Register SharedPreferences, CacheHelper, and ApiInterceptor in GetIt
    - Update setupServiceLocator to be async for SharedPreferences initialization
    - Remove duplicate CacheHelper initialization from main.dart
    - Configure DioConsumer to use ApiInterceptor

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hungry_app/core/cache/cache_helper.dart';
 import 'package:hungry_app/core/networking/api/api_consumer.dart';
+import 'package:hungry_app/core/networking/api/api_interceptor.dart';
 import 'package:hungry_app/core/networking/api/dio_consumer.dart';
 import 'package:hungry_app/feature/home/data/data_sources/home_remote_data_sources.dart';
 import 'package:hungry_app/feature/home/data/repos/home_reposotory_impl.dart';
@@ -9,15 +11,37 @@ import 'package:hungry_app/feature/home/doman/usecases/home_usecases.dart';
 import 'package:hungry_app/feature/home/doman/usecases/product_with_id_use_cases.dart';
 import 'package:hungry_app/feature/home/presentation/cubit/product_cubit.dart';
 import 'package:hungry_app/feature/home/presentation/cubit/product_details_cubit.dart';
+import 'package:hungry_app/feature/profile/data/datasources/profile_remote_data_source.dart';
+import 'package:hungry_app/feature/profile/data/repos/profile_repository_impl.dart';
+import 'package:hungry_app/feature/profile/domain/repo/profile_repo.dart';
+import 'package:hungry_app/feature/profile/domain/usecases/get_user_profile_usecase.dart';
+import 'package:hungry_app/feature/profile/presentation/cubit/profile_cubit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final getIt = GetIt.instance;
 
-void setupServiceLocator() {
+Future<void> setupServiceLocator() async {
+  // Core - SharedPreferences (must be async)
+  final sharedPreferences = await SharedPreferences.getInstance();
+  getIt.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
+
+  // Core - CacheHelper
+  getIt.registerLazySingleton<CacheHelper>(
+    () => CacheHelper(sharedPreferences: getIt<SharedPreferences>()),
+  );
+
   // Core - Dio
   getIt.registerLazySingleton<Dio>(() => Dio());
 
+  // Core - API Interceptor
+  getIt.registerLazySingleton<ApiInterceptor>(
+    () => ApiInterceptor(cacheHelper: getIt<CacheHelper>()),
+  );
+
   // Core - API Consumer
-  getIt.registerLazySingleton<ApiConsumer>(() => DioConsumer(getIt<Dio>()));
+  getIt.registerLazySingleton<ApiConsumer>(
+    () => DioConsumer(getIt<Dio>(), apiInterceptor: getIt<ApiInterceptor>()),
+  );
 
   // Home Feature - Data Sources
   getIt.registerLazySingleton<HomeRemoteDataSource>(
@@ -49,5 +73,27 @@ void setupServiceLocator() {
       getIt<ProductWithIdUseCases>(),
       getIt<HomeRemoteDataSource>(),
     ),
+  );
+
+  // Profile Feature - Data Sources
+  getIt.registerLazySingleton<ProfileRemoteDataSource>(
+    () => ProfileRemoteDataSourceImpl(api: getIt<ApiConsumer>()),
+  );
+
+  // Profile Feature - Repositories
+  getIt.registerLazySingleton<ProfileRepo>(
+    () => ProfileRepositoryImpl(
+      remoteDataSource: getIt<ProfileRemoteDataSource>(),
+    ),
+  );
+
+  // Profile Feature - Use Cases
+  getIt.registerLazySingleton<GetUserProfileUseCase>(
+    () => GetUserProfileUseCase(profileRepo: getIt<ProfileRepo>()),
+  );
+
+  // Profile Feature - Cubit (Factory - new instance each time)
+  getIt.registerFactory<ProfileCubit>(
+    () => ProfileCubit(getIt<GetUserProfileUseCase>()),
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:hungry_app/core/cache/cache_helper.dart';
 import 'package:hungry_app/core/di/di.dart';
 import 'package:hungry_app/core/functions/on_generate_route.dart';
 import 'package:hungry_app/feature/splash/presentation/splash_view.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-late CacheHelper cacheHelper;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  // Initialize GetIt Service Locator
-  setupServiceLocator();
-
-  // Initialize SharedPreferences
-  final sharedPreferences = await SharedPreferences.getInstance();
-  cacheHelper = CacheHelper(sharedPreferences: sharedPreferences);
+  // Initialize GetIt Service Locator (includes CacheHelper setup)
+  await setupServiceLocator();
 
   runApp(const HungryApp());
 }
